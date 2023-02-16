ddba7f7f6ed40fc54c3e969e62e19c77848fed33
Samuel Audu <mrsamueladi.com>
2023-02-16 09:51:05 +0100

    created the splash screen

---
+++ lib/views/splash screen/logo_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LogoScreen extends StatelessWidget {
+  const LogoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xff0f111f),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Container(
+                height: 134,
+                width: 134,
+                child: Image.asset('images/dclmlogo.png'),
+              ),
+              //DLCM Radio App text
+              Text('DCLM Radio App',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 20),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
